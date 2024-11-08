691f06af7851070ed6f5c2a4ddf7e9f3adab546f
SHEIKH DANISH <danish1521@gmail.com>
2024-11-08 16:25:45 +0530

    controlflow

---
+++ controlflow.py
@@ -0,0 +1,67 @@
+''' 1) Sequancial
+   2) conditional
+                1) if
+                2) if-else
+                3)if-elif
+                4)if-elif-else
+   3)Iterative/looping
+                 1) while --- infinite time(increment or decriment nahi hoga)
+                 2)for
+
+  4) Transfar statement
+                1)continue(keyword)
+                2)brack(keyword)
+                3)pass (keyword)
+
+
+----------------------------------------------------------------------------------------------------
+flow chart :-
+              
+x=10
+y=10
+if x==y:
+print("Equal")
+print("hello")        
+
+
+syntex:-
+
+   if(condition):
+   {
+   
+   
+   }
+
+                
+'''
+# x=20
+# y=20
+# if x==y:
+#  print("Equal")
+# else: 
+#  print("hello")        
+
+
+
+
+# x=20
+# y=20
+# if x==y:
+#  print("Equal")
+# else:
+#  print("Hello")    
+
+x=int(input("Enter one no= "))
+y=int(input("Enter 2nd.no="))
+print("1 Add\n 2.sub\n 3.Div\n 4.malti")
+
+n=int(input("Enter or. ch="))
+if n==1:
+ print(x+y)
+elif n==2:
+  print(x-y)
+elif n==3:
+     print(x/y)
+elif n==4:
+     print(x*y)
+
