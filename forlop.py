50232a134dbdf44f8672f47747e5e89fb7590fbe
SHEIKH DANISH <danish1521@gmail.com>
2024-11-15 15:41:50 +0530

    for

---
+++ forlop.py
@@ -0,0 +1,88 @@
+'''
+for i in collection:
+                  1)string
+                  2)list
+                  3)tuple
+                  4)set
+                  5)dict
+                  6)range
+
+
+
+
+'''
+# n=int(input("Enter number="))
+# for i in range (1,n+1):
+#     if i<=(n-1):     #natural number n
+#         print(i,end = ' , ')
+#     else:
+#         print(i)    
+     
+
+
+# n=int(input("Enter number="))
+# sum=0
+# for i in range (1,n+1):
+#     sum=sum+i
+#     if i<=(n-1):     #natural number n ka sum
+#         print(i,end = ' + ')
+#     else:
+#         print(i,end = ' = ')    
+# print(sum)        
+
+
+# n=int(input("Enter number="))
+# sum=0
+# for i in range (2,n+1,2):
+#     sum=sum+i
+#     if i<=(n-2):     #evan ka sum
+#         print(i,end = ' + ')
+#     else:
+#         print(i,end = ' = ')    
+# print(sum)   
+
+# n=int(input("Enter number="))
+# sum=0
+# for i in range (1,n+1,2):
+#     sum=sum+i
+#     if i<=(n-2):     #odd ka sum
+#         print(i,end = ' + ')
+#     else:
+#         print(i,end = ' = ')    
+# print(sum)   
+
+
+
+# n=int(input("Enter number="))
+
+# for i in range (1,n+1):
+    
+#     if i<=(n-1 *2):     #10 even number print
+#         print(2*i,end = ' , ')
+#     else:
+#         print(2*i)    
+   
+
+# n=int(input("Enter number="))
+
+# for i in range (1,n+1):
+    
+#     if i<=(n-1): 
+#                     #10 odd number print
+#         print(2*i-1,end = ' , ')
+#     else:
+#         print(2*i-1)    
+   
+
+# n=(input("Enter your string="))
+# v=0
+# c=0
+# for i in n:
+#     x=['a','e','i','o','u','A','E','I','O','U']
+#     if i in x:
+#         v=v+1      
+#     else:
+#         c=c+1     #count const and vowel
+# print("Vowel=",v)
+# print("Const=",c)               
+
