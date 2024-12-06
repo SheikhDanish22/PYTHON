6f17ff81b54d67aa7f94953e5db817b5d18d56b9
SHEIKH DANISH <danish1521@gmail.com>
2024-12-06 23:30:35 +0530

    MPL

---
+++ th.py
@@ -0,0 +1,8 @@
+from first import multi
+from first import add
+p=int(input("Enter any num="))
+q=int(input("Enter any num="))
+multi=multi(p,q)
+print("Multi=",multi)
+add=add(p,q)
+print("add=",add)
