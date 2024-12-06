647cc7132877ed393cbd2a26c53f19e851ba7f48
SHEIKH DANISH <danish1521@gmail.com>
2024-12-06 23:30:14 +0530

    MPL

---
+++ sec.py
@@ -0,0 +1,9 @@
+import first 
+p=int(input("Enter any num="))
+q=int(input("Enter any num="))
+add=first.add(p,q)
+print("Add=",add)
+sub=first.sub(p,q)
+print("Sub=",sub)
+multi=first.multi(5,3)
+print(multi)
