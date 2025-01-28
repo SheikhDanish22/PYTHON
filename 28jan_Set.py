70a24a36b16cd4370ae7666da9610b1dde32140b
SHEIKH DANISH <danish1521@gmail.com>
2025-01-28 23:20:03 +0530

    Set

---
+++ 28jan_Set.py
@@ -0,0 +1,56 @@
+# my_set={'danish',10,20,'rahul',30,40,'raj'}
+# print(my_set)
+# print(type(my_set))
+
+
+s1={'Neeraj','Raj','Rahul'}
+# s2={10,20,30,40,50}
+# print(max(s1),min(s1))
+# print(max(s2),min(s2),sum(s2))
+# print(s1.)
+
+
+
+# s1.clear()
+# print(s1)
+
+
+
+# s1.add(50)
+# print(s1) 
+
+# l1=[2,4,6,8,10]
+# s1.update(l1)
+# print(s1)
+
+# s1.pop()
+# print(s1)
+# s1.remove(10)
+# print(s1)
+# s1.discard(10)
+# print(s1)
+
+
+
+
+a={1,2,3,4,5,6}
+b={3,6,7,8}
+# print(a.union(b))
+
+
+# print(a.intersection(b))
+# print(a.intersection_update(b))
+# print(a)
+
+'''
+print(a.difference(b))
+
+print(a)
+
+
+a.difference_update(b)
+print(a)'''
+
+
+
+
