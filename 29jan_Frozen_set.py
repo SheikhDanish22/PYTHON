7d2b808d9dc043eb6061db4cec246f59d36ae190
SHEIKH DANISH <danish1521@gmail.com>
2025-01-29 23:24:34 +0530

    Frozen_set

---
+++ 29jan_Frozen_set.py
@@ -0,0 +1,35 @@
+s='danish'
+
+x=frozenset(s)
+print(x)
+print(type(x))
+# y=x.copy()
+# print("y=",y)
+
+
+# a={2,4,6,8}
+# b={1,3,5,7,6,8}
+# x=frozenset(a)
+# y=frozenset(b)
+# print(x.union(y))
+# print(x.intersection(y))
+# print(x.difference(y))
+# print(x.symmetric_difference(y))
+# print(x.isdisjoint(y))
+
+
+
+# p={1,2,3}
+# q={1,2,3,4}
+# print(p.isdisjoint(q))
+
+
+# print(p.issubset(q))
+# print(q.issuperset(p))
+
+
+
+A="Danish"
+B=frozenset(A)
+print(B)
+print(type(B))
