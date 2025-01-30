f3da791b87a6dc7392a443529a41e076066b2383
SHEIKH DANISH <danish1521@gmail.com>
2025-01-30 23:43:58 +0530

    if_else

---
+++ 30jan_ControlStatement.py
@@ -0,0 +1,33 @@
+# x=int(input("Enter your number="))
+# if x%2==0:
+#     print("it is a even number")
+
+# else:
+#     print("odd number")
+
+
+
+# x=int(input("enter first number="))
+# y=int(input("enter first number="))
+# if x>y:
+#     print("x is greater")
+
+# else:
+#     print(f'{y} is greater')    # f string print ka pettern
+
+    
+# x=int(input("enter your age="))
+# if x>=18:
+#     print("you can vote")
+
+# else:
+#     print("you can't vote")        
+
+x=int(input("Enter no.1="))
+y=int(input("Enter no.2="))
+z=int(input("Enter no.3="))
+if(x>y and x>z and y>z):
+    print(x)
+    print(y)
+    print(z)
+    
