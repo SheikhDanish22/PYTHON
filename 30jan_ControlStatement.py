6b285598796e689ec77e0b8695d4397a07e1da66
SHEIKH DANISH <danish1521@gmail.com>
2025-01-31 21:21:09 +0530

    if_else

--- 30jan_ControlStatement.py
+++ 30jan_ControlStatement.py
@@ -23,11 +23,45 @@
 # else:
 #     print("you can't vote")        
 
+
+
+# 3 value compare
+# x=int(input("Enter no.1="))
+# y=int(input("Enter no.2="))
+# z=int(input("Enter no.3="))
+# if(x>y and x>z ):
+#      print(f'{x} is greate')
+# else:
+#     if(y>z and y>x):
+#         print(f'{y} is greater')
+#     else:
+#         print("z is greater",z)        
+
+
+
 x=int(input("Enter no.1="))
 y=int(input("Enter no.2="))
 z=int(input("Enter no.3="))
-if(x>y and x>z and y>z):
-    print(x)
-    print(y)
-    print(z)
+if (x>y and x>z):
+    print (f'{x}')
+elif (y>z and y>x):
+    print(f'{y}')
+elif(z>x and z>y):
+    print(f'{z}')
+elif(x==y and y==z):
+    print(f'{x} and {y} and {z} all value are equal')          
+elif (x==y):
+    print(f'{x} and {y} both are equal')
+elif(y==z):
+    print(f'{y} and {z} both are equal')
+elif(z==x):
+    print(f'{z}and {x} both are equal') 
+
+else:
+    print("plz enter valid value")             
+   
+
+   
+    
+    
     
