fd69363a04f919ed3bbeed045aad4228d19225d6
SHEIKH DANISH <danish1521@gmail.com>
2025-02-04 13:38:51 +0530

    for_while

---
+++ IterativeStatement.py
@@ -0,0 +1,18 @@
+#for loop infinte nahi chalta.
+
+# n=int(input("enter number"))
+# i=1
+# while i<=n:
+#     print(i)
+#     i=i+1      
+
+
+
+# 10 even number
+n=int(input("enter number"))
+i=1
+while i<=n:
+    x=2*i
+    print(f'2*{i}={x}',end=' ')
+    i=i+1
+
