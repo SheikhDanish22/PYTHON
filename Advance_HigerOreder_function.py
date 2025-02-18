4fcfbc6607173292bf66bb7fd30dd93fda643972
SHEIKH DANISH <danish1521@gmail.com>
2025-02-18 15:49:53 +0530

    Higer_order_function

---
+++ Advance_HigerOreder_function.py
@@ -0,0 +1,30 @@
+
+#map()
+
+# def add(x):
+#     if(x%2==0):
+#         return x+1
+#     else:
+#         return x+2
+    
+# l1=[1,2,3,4,5,6,7,8,9,10]
+# res=map(add,l1)
+# print(tuple(res))    
+
+
+
+# def add(x,y,z):
+#     return x+y+z
+# l1=[2,4,6]
+# l2=[1,2,3]
+# l3=[4,5,6,7,8]
+# res=map(add,l1,l2,l3)
+# print(tuple(res))
+
+
+# def pow(x,y):
+#     return x**y
+# l1=eval(input("enter number:"))
+# l2=eval(input("Enter number:"))
+# res=map(pow,l1,l2)
+# print(list(res))
